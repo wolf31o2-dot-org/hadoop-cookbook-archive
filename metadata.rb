2d8a063db21c04e68355b3cae97c722bd6e55543
Andrew Crump <andrew@kotirisoftware.com>
2012-04-13 17:17:46 +0100

    Increment version for development.

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer_email  "cookbooks@opscode.com"
 license           "Apache 2.0"
 description       "Installs hadoop and sets up basic cluster per Cloudera's quick start docs"
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version           "0.8.1"
+version           "0.8.3"
 depends           "java"
 
 recipe "hadoop", "Installs hadoop from Cloudera's repo"
