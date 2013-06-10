bf8cc1dfa8848ce82159be6343466fdaeced93d4
Ahmed El Gamil <ahmed@manhag.org>
2013-06-10 20:03:02 +0300

    Update metadata.rb

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email  "cookbooks@opscode.com"
 license           "Apache 2.0"
 description       "Installs hadoop and sets up basic cluster per Cloudera's quick start docs"
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version           "0.8.2"
+version           "0.8.1"
 depends           "java"
 depends           "apt"
 
